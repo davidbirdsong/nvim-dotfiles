5ab8078796f2b6bf1d9b5ac36c612f0500dce40f
david.birdsong <david.birdsong@netlify.com>
2024-11-11 11:20:22 -0800

    set lua style to 4 indent spaces

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -1,8 +1,7 @@
 return {
-
-  {
-    "folke/tokyonight.nvim",
-    lazy = true,
-    opts = { style = "moon" },
-  },
+    {
+        "folke/tokyonight.nvim",
+        lazy = true,
+        opts = { style = "moon" },
+    },
 }
